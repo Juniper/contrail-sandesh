a33849df71faa0fb6752f3504bdac37ff9e5d545
Megh Bhatt <meghb@juniper.net>
2014-11-26 14:15:40 -0800

    Send NodeStatusUve and ProcessStatus on ConnectionState Update and Delete instead of from a periodic so that the contrail-status output can show accurate information quickly. Added unit tests for the connection info code in C++ and python. Closes-Bug: #1392443
    Change-Id: I19c5f9d01eb38313d1ba3319728f29cf0cac6862

---
+++ library/python/pysandesh/test/conn_info_test.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+
+#
+# Copyright (c) 2014 Juniper Networks, Inc. All rights reserved.
+#
+
+#
+# conn_info_test
+#
+
+import sys
+import socket
+import unittest
+import test_utils
+
+sys.path.insert(1, sys.path[0]+'/../../../python')
+from pysandesh.gen_py.process_info.constants import ConnectionTypeNames, \
+    ConnectionStatusNames, ProcessStateNames
+from pysandesh.gen_py.process_info.ttypes import ConnectionInfo, \
+    ProcessStatus, ProcessState, ConnectionType, ConnectionStatus
+from pysandesh.connection_info import ConnectionState
+from gen_py.connection_info_test.ttypes import NodeStatusTestUVE, NodeStatusTest
+from pysandesh.sandesh_base import Sandesh
+
+class ConnInfoTest(unittest.TestCase):
+
+    def setUp(self):
+        self.maxDiff = None
+        self._sandesh = Sandesh()
+        http_port = test_utils.get_free_port()
+        self._sandesh.init_generator('conn_info_test', socket.gethostname(),
+            'Test', 'Test', None, 'conn_info_test_ctxt', http_port)
+    #end setUp
+
+    def _check_process_status_cb(self, vcinfos):
+        self._expected_vcinfos.sort()
+        vcinfos.sort()
+        self.assertEqual(self._expected_vcinfos, vcinfos)
+        return (ProcessState.FUNCTIONAL, '')
+    #end _check_process_status_cb
+
+    def _update_conn_info(self, name, status, description, vcinfos):
+        cinfo = ConnectionInfo()
+        cinfo.name = name
+        cinfo.status = ConnectionStatusNames[status]
+        cinfo.description = description
+        cinfo.type = ConnectionTypeNames[ConnectionType.TEST]
+        cinfo.server_addrs = ['127.0.0.1:0']
+        vcinfos.append(cinfo)
+    #end _update_conn_info
+
+    def _update_conn_state(self, name, status, description, vcinfos):
+        self._expected_vcinfos = vcinfos
+        ConnectionState.update(ConnectionType.TEST, name, status,
+            ['127.0.0.1:0'], description)
+    #end _update_conn_state
+
+    def _delete_conn_info(self, name, vcinfos):
+        return [cinfo for cinfo in vcinfos if cinfo.name != name]
+    #end _delete_conn_info
+
+    def _delete_conn_state(self, name, vcinfos):
+        self._expected_vcinfos = vcinfos
+        ConnectionState.delete(ConnectionType.TEST, name)
+    #end _delete_conn_state
+
+    def test_basic(self):
+        ConnectionState.init(sandesh = self._sandesh, hostname = "TestHost",
+            module_id = "TestModule", instance_id = "0",
+            status_cb = self._check_process_status_cb,
+            uve_type_cls = NodeStatusTestUVE,
+            uve_data_type_cls = NodeStatusTest)
+        vcinfos = []
+        self._update_conn_info("Test1", ConnectionStatus.UP, "Test1 UP",
+            vcinfos)
+        self._update_conn_state("Test1", ConnectionStatus.UP, "Test1 UP",
+            vcinfos)
+        self._update_conn_info("Test2", ConnectionStatus.UP, "Test2 UP",
+            vcinfos)
+        self._update_conn_state("Test2", ConnectionStatus.UP, "Test2 UP",
+            vcinfos)
+        vcinfos = self._delete_conn_info("Test2", vcinfos)
+        self._delete_conn_state("Test2", vcinfos)
+    #end test_basic
+
+    def test_callback(self):
+        vcinfos = []
+        self._update_conn_info("Test1", ConnectionStatus.UP, "Test1 UP",
+            vcinfos);
+        (pstate, message) = ConnectionState.get_process_state_cb(vcinfos)
+        self.assertEqual(ProcessState.FUNCTIONAL, pstate)
+        self.assertEqual('', message)
+        self._update_conn_info("Test2", ConnectionStatus.DOWN, "Test2 DOWN",
+            vcinfos);
+        (pstate, message) = ConnectionState.get_process_state_cb(vcinfos)
+        self.assertEqual(ProcessState.NON_FUNCTIONAL, pstate)
+        self.assertEqual("Test:Test2 connection down", message);
+        self._update_conn_info("Test3", ConnectionStatus.DOWN, "Test3 DOWN",
+            vcinfos);
+        (pstate, message) = ConnectionState.get_process_state_cb(vcinfos);
+        self.assertEqual(ProcessState.NON_FUNCTIONAL, pstate);
+        self.assertEqual("Test:Test2, Test:Test3 connection down", message);
+    #end test_callback
+
+#end ConnInfoTest
+
+if __name__ == '__main__':
+    unittest.main()
