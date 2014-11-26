d037fc4d0a2fd136d402985a86b6911775292898
Megh Bhatt <meghb@juniper.net>
2014-11-26 14:15:40 -0800

    Send NodeStatusUve and ProcessStatus on ConnectionState Update and Delete instead of from a periodic so that the contrail-status output can show accurate information quickly. Added unit tests for the connection info code in C++ and python. Closes-Bug: #1392443
    Change-Id: I19c5f9d01eb38313d1ba3319728f29cf0cac6862

--- library/python/pysandesh/connection_info.py
+++ library/python/pysandesh/connection_info.py
@@ -14,37 +14,35 @@ from gen_py.process_info.ttypes import ConnectionInfo, \
     ProcessStatus, ProcessState, ConnectionStatus
 
 class ConnectionState(object):
-    _CONN_INFO_INTVL = 60
     _sandesh = None 
     _connection_map = {}
     _hostname = None 
     _module_id = None
     _instance_id = None
     _status_cb = None
-    _conn_info_timer = None
     _uve_type_cls = None
     _uve_data_type_cls = None
 
     @staticmethod
-    def _conn_info_timer_expiry_handler():
-        while True:
-            conn_infos = ConnectionState._connection_map.values()
-            (process_state, message) = \
-                ConnectionState._status_cb(conn_infos)
-            process_status = ProcessStatus(
-                module_id = ConnectionState._module_id,
-                instance_id = ConnectionState._instance_id,
-                state = ProcessStateNames[process_state],
-                connection_infos = conn_infos,
-                description = message)
-            uve_data = ConnectionState._uve_data_type_cls(
-                name = ConnectionState._hostname,
-                process_status = [process_status])
-            uve = ConnectionState._uve_type_cls(data = uve_data,
-                    sandesh = ConnectionState._sandesh)
-            uve.send(sandesh = ConnectionState._sandesh)
-            gevent.sleep(ConnectionState._CONN_INFO_INTVL)
-    #end _conn_info_timer_expiry_handler
+    def _send_uve():
+        if not ConnectionState._status_cb:
+            return
+        conn_infos = ConnectionState._connection_map.values()
+        (process_state, message) = \
+            ConnectionState._status_cb(conn_infos)
+        process_status = ProcessStatus(
+            module_id = ConnectionState._module_id,
+            instance_id = ConnectionState._instance_id,
+            state = ProcessStateNames[process_state],
+            connection_infos = conn_infos,
+            description = message)
+        uve_data = ConnectionState._uve_data_type_cls(
+            name = ConnectionState._hostname,
+            process_status = [process_status])
+        uve = ConnectionState._uve_type_cls(data = uve_data,
+                sandesh = ConnectionState._sandesh)
+        uve.send(sandesh = ConnectionState._sandesh)
+    #end _send_uve
 
     @staticmethod
     def init(sandesh, hostname, module_id, instance_id, status_cb,
@@ -56,10 +54,6 @@ class ConnectionState(object):
         ConnectionState._status_cb = status_cb
         ConnectionState._uve_type_cls = uve_type_cls
         ConnectionState._uve_data_type_cls = uve_data_type_cls
-        if ConnectionState._conn_info_timer is None:
-            ConnectionState._conn_info_timer = gevent.spawn_later(
-                ConnectionState._CONN_INFO_INTVL,
-                ConnectionState._conn_info_timer_expiry_handler)
     #end init
 
     @staticmethod
@@ -91,12 +85,14 @@ class ConnectionState(object):
                                    description = message,
                                    server_addrs = server_addrs)
         ConnectionState._connection_map[conn_key] = conn_info
+        ConnectionState._send_uve()
     #end update
 
     @staticmethod
     def delete(conn_type, name):
         conn_key = (conn_type, name)
         ConnectionState._connection_map.pop(conn_key, 'None')
+        ConnectionState._send_uve()
     #end delete
 
 #end class ConnectionState
