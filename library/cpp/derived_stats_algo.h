7247c12e70851c20d023e62692d2761106ce2361
Anish Mehta <amehta00@gmail.com>
2016-09-01 00:56:37 -0700

    Change SUM and AVG DerivedStats to be time-range based. Sandesh Message Stats are changed to us this instead of periodic processing. Closes-Bug: 1619156
    Change-Id: If2172bbe3b8b95df543fe79e999f0076eb5cba30

--- library/cpp/derived_stats_algo.h
+++ library/cpp/derived_stats_algo.h
@@ -16,63 +16,18 @@
 #include <cmath>
 #include <sstream>
 #include <sandesh/derived_stats_results_types.h>
+#include <sandesh/derived_stats.h>
 #include <boost/assign/list_of.hpp>
 #include <boost/scoped_ptr.hpp>
 
 using std::vector;
 using std::map;
+using std::pair;
 using std::make_pair;
 using std::string;
 
 namespace contrail {
 namespace sandesh {
-
-template<class CatElemT, class CatResT>
-class DSCategoryCount {
-};
-
-template<>
-class DSCategoryCount<CategoryResult,CategoryResult> {
-  public: 
-    DSCategoryCount(const std::string &annotation) {}
-
-    typedef map<std::string, uint64_t> CatResT;
-    CatResT agg_counts_;
-    CatResT diff_counts_;
-
-    bool FillResult(CategoryResult &res) const {
-        res.set_counters(diff_counts_);
-        return !diff_counts_.empty();
-    }
-
-    void Update(const CategoryResult& raw) {
-        diff_counts_.clear();
-        for (CatResT::const_iterator it = raw.get_counters().begin();
-                it != raw.get_counters().end(); it++) {
-
-            // Is there anything to count?
-            if (!it->second) continue;
-          
-            CatResT::iterator mit =
-                    agg_counts_.find(it->first);
- 
-            if (mit==agg_counts_.end()) {
-                // This is a new category
-                diff_counts_.insert(make_pair(it->first, it->second));
-                agg_counts_.insert(make_pair(it->first, it->second));
-            } else {
-                assert(it->second >= mit->second);
-                uint64_t diff = it->second - mit->second;
-                if (diff) {
-                    // If the count for this category has changed,
-                    // report the diff and update the aggregate
-                    diff_counts_.insert(make_pair(it->first, diff));
-                    mit->second = it->second;
-                }
-            }
-        }
-    }
-};
 
 // Interface for all Anomaly Detection algorithms
 // That can be plugged into the DSAnomaly DerivedStat
@@ -155,24 +110,23 @@ class DSAnomaly {
         impl_.reset(NULL);
     }
 
-    bool FillResult(AnomalyResT &res) const {
-        bool ret = true;
+    DSReturnType FillResult(AnomalyResT &res) const {
+        DSReturnType ret = DSR_OK;
         if (impl_) {
-            ret = impl_->FillResult(res);
+            if (!impl_->FillResult(res)) ret = DSR_SKIP;
             // We should have cleared impl_ if there was a parsing error
             // with the DSAnomaly config
             assert(error_.empty());
         }
-        if (ret) {
-            res.set_samples(samples_);
-            res.set_algo(algo_);
-            res.set_config(config_);
-            if (!error_.empty()) res.set_error(error_);
-        }
+        
+	res.set_samples(samples_);
+	res.set_algo(algo_);
+	res.set_config(config_);
+	if (!error_.empty()) res.set_error(error_);
         return ret;
     }
 
-    void Update(const ElemT& raw) {
+    void Update(const ElemT& raw, uint64_t mono_usec) {
         samples_++;
         if (impl_) impl_->Update(raw);
     }
@@ -188,7 +142,8 @@ template <typename ElemT, class EWMResT>
 class DSEWM {
   public:
     DSEWM(const std::string &annotation):
-                mean_(0), variance_(0), samples_(0) {
+                mean_(0), variance_(0),
+                sigma_(0), stddev_(0), samples_(0) {
         alpha_ = (double) strtod(annotation.c_str(), NULL);
         if (alpha_ == 0) {
             error_ = std::string("Disabled");
@@ -208,18 +163,18 @@ class DSEWM {
     uint64_t samples_;
     std::string error_;
 
-    bool FillResult(EWMResT &res) const {
+    DSReturnType FillResult(EWMResT &res) const {
 	res.set_samples(samples_);
         if (!error_.empty()) {
             res.set_error(error_);
-            return true;
+            return DSR_OK;
         }
         res.set_mean(mean_);
         res.set_stddev(stddev_);
         res.set_sigma(sigma_);
-        return true;
+        return DSR_OK;
     }
-    void Update(const ElemT& raw) {
+    void Update(const ElemT& raw, uint64_t mono_usec) {
         samples_++;
         if (!error_.empty()) return;
         variance_ = (1-alpha_)*(variance_ + (alpha_*pow(raw-mean_,2)));
@@ -239,14 +194,14 @@ class DSChange {
     ElemT value_;
     ElemT prev_;
 
-    bool FillResult(NullResT &res) const {
-        assert(init_);
+    DSReturnType FillResult(NullResT &res) const {
+        if (!init_) return DSR_INVALID;
         res = value_;
-	if (prev_ == value_) return false;
-	else return true;
+	if (prev_ == value_) return DSR_SKIP;
+	else return DSR_OK;
     }
 
-    void Update(const ElemT& raw) {
+    void Update(const ElemT& raw, uint64_t mono_usec) {
         if (init_) prev_ = value_;
         value_ = raw;
         init_ = true;
@@ -259,13 +214,13 @@ class DSNon0 {
     DSNon0(const std::string &annotation) {}
     ElemT value_;
 
-    bool FillResult(NullResT &res) const {
+    DSReturnType FillResult(NullResT &res) const {
 	res = value_;
-	if (value_ == 0) return false;
-	return true;
+	if (value_ == 0) return DSR_SKIP;
+	else return DSR_OK;
     }
 
-    void Update(const ElemT& raw) {
+    void Update(const ElemT& raw, uint64_t mono_usec) {
         value_ = raw;
     }
 };
@@ -276,11 +231,11 @@ class DSNone {
     DSNone(const std::string &annotation) {}
     ElemT value_;
 
-    bool FillResult(NullResT &res) const {
+    DSReturnType FillResult(NullResT &res) const {
         res = value_;
-        return true;
+        return DSR_OK;
     }
-    void Update(const ElemT& raw) {
+    void Update(const ElemT& raw, uint64_t mono_usec) {
         value_ = raw;
     }
 };
@@ -292,85 +247,101 @@ class DSNull {
     ElemT value_;
     uint64_t samples_;
 
-    bool FillResult(NullResT &res) const {
+    DSReturnType FillResult(NullResT &res) const {
         res.set_samples(samples_);
         res.set_value(value_);
-        return true;
+        return DSR_OK;
     }
-    void Update(const ElemT& raw) {
+    void Update(const ElemT& raw, uint64_t mono_usec) {
         samples_++;
         value_ = raw;
-    }
-};
-
-/* Deprecated: use metric="agg" on raw stat instead */
-template <typename ElemT, class DiffResT>
-class DSDiff {
-  public:
-    DSDiff(const std::string &annotation) : init_(false) {}
-    bool init_;
-    ElemT agg_;
-    ElemT diff_;
-
-    bool FillResult(DiffResT &res) const {
-        ElemT empty;
-        if (!init_) return false;
-        if (diff_ == empty) return false;
-        res = diff_;
-        return true;
-    }
-    void Update(const ElemT& raw) {
-        if (!init_) {
-            diff_ = raw;
-            agg_ = raw;
-        } else {
-            diff_ = raw - agg_;
-            agg_ = agg_ + diff_;
-        }
-        init_ = true;
     }
 };
 
 template <typename ElemT, class SumResT> 
 class DSSum {
   public:
-    DSSum(const std::string &annotation): samples_(0) {
+    DSSum(const std::string &annotation): samples_(0), shifter_(0),
+            start_tbin_(0), last_tbin_(0) {
+
         if (annotation.empty()) {
-            history_count_ = 0;
+            range_usecs_ = 0;
         } else {
-            history_count_ = (uint64_t) strtoul(annotation.c_str(), NULL, 10);
+            range_usecs_ = ((uint64_t) strtoul(annotation.c_str(), NULL, 10)) * 1000000;
+            // We want each time bucket to represent between
+            // 1/128th and 1/256th of the entire range
+            // This ensures that there will never be more than 256 buckets
+            while ((1 << (shifter_ + 8)) < range_usecs_) shifter_++;   
         }
     }
     uint64_t samples_;
     SumResT value_;
-    SumResT prev_;
-    vector<ElemT> history_buf_;
-    uint64_t history_count_;
+    map<uint64_t, pair<uint64_t,ElemT> > history_buf_;
+    uint64_t range_usecs_;
+    uint8_t shifter_;
+    uint64_t start_tbin_;
+    uint64_t last_tbin_;
 
-    virtual bool FillResult(SumResT &res) const {
-        if (!samples_) return false;
+    virtual DSReturnType FillResult(SumResT &res) const {
+        static SumResT empty_val;
+        if (!samples_) {
+            res = empty_val;
+            return DSR_INVALID;
+        }
+        if (range_usecs_) {
+            // At least one time-range's worth of samples must be seen
+            // before we can report a SUM
+            uint64_t end_range =
+                    ((start_tbin_ + range_usecs_) >> shifter_) << shifter_;
+            if (end_range > last_tbin_) {
+                res = empty_val;
+                return DSR_INVALID;
+            } else {
+            }
+        }
         res = value_;
-        if (history_count_ && (value_ == prev_)) return false;
-        else return true;
+        return DSR_OK;
     }
 
-    virtual void Update(const ElemT& raw) {
+    virtual void Purge(uint64_t mono_usec) {
+
+        for (typename map<uint64_t, pair<uint64_t,ElemT> >::iterator it = 
+                history_buf_.begin(); it!= history_buf_.end(); ) {
+            uint64_t end_range = ((it->first + range_usecs_) >> shifter_) << shifter_;
+            if (end_range <= mono_usec) {
+                value_ = value_ - it->second.second;
+                samples_ = samples_ - it->second.first;
+                history_buf_.erase(it++);
+            } else {
+                ++it;
+            }
+        } 
+    }
+
+    virtual void Update(const ElemT& raw, uint64_t mono_usec) {
         if (!samples_) {
             value_ = raw;
         } else {
-            prev_ = value_;
             value_ = value_ + raw;
         }
-        if (history_count_) {
-            // if 'history_count_' is non-0, we need to aggregate only
-            // the last 'history_count_' elements
-            if (samples_ >= history_count_) {
-                // We must substract the oldest entry, if it exists
-                value_ = value_ - history_buf_[samples_ % history_count_];
-                // Now record the latest update, so we can subtract when needed
-                history_buf_[samples_ % history_count_] = raw;
+        if (range_usecs_) {
+            // if 'range_usecs_' is non-0, we need to aggregate only
+            // the last 'range_usecs_' worth of elements
+            uint64_t tbin = (mono_usec >> shifter_) << shifter_;
+            if (!start_tbin_) start_tbin_ = tbin;
+            last_tbin_ = tbin;
+
+            // Subtract old entries, if there are any
+            Purge(tbin);
+
+            // Record the new update, so we can subtract when needed
+            typename map<uint64_t, pair<uint64_t,ElemT> >::iterator ut =
+                    history_buf_.find(tbin);
+            if (ut == history_buf_.end()) {
+                history_buf_[tbin] = make_pair(1,raw);
             } else {
-                history_buf_.push_back(raw);
+                ut->second.second = ut->second.second + raw;
+                ut->second.first++;
             }
         }
         samples_++;
@@ -382,17 +353,14 @@ class DSAvg : public DSSum<ElemT,AvgResT> {
   public:
     DSAvg(const std::string &annotation): DSSum<ElemT,AvgResT>(annotation) {}
 
-    virtual bool FillResult(AvgResT &res) const {
-        if (!DSSum<ElemT,AvgResT>::samples_) return false;
-        if (DSSum<ElemT,AvgResT>::history_count_) {
-	    uint64_t div =
-                    ((DSSum<ElemT,AvgResT>::samples_ >=DSSum<ElemT,AvgResT>::history_count_) ?
-		     DSSum<ElemT,AvgResT>::history_count_ : DSSum<ElemT,AvgResT>::samples_);
-	    res = DSSum<ElemT,AvgResT>::value_ / div;
+    virtual DSReturnType FillResult(AvgResT &res) const {
+        AvgResT sres;
+        if (DSSum<ElemT,AvgResT>::FillResult(sres) == DSR_OK) {
+            res = sres / DSSum<ElemT,AvgResT>::samples_;
+            return DSR_OK;
         } else {
-            res = DSSum<ElemT,AvgResT>::value_ / DSSum<ElemT,AvgResT>::samples_;
+            return DSR_INVALID;
         }
-        return true;
     }
 };
 
