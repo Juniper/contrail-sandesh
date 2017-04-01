a029d5857c44b513b9a90cc5069d0a554a24b329
Megh Bhatt <meghb@juniper.net>
2017-03-31 18:48:08 -0700

    Fix compilation failure in sandesh_send_queue_test
    Change-Id: Ibf96335bc959c12c0578cbdc329ec6f4dc3fb74f
    Closes-Bug: #1678350

--- library/cpp/test/sandesh_send_queue_test.cc
+++ library/cpp/test/sandesh_send_queue_test.cc
@@ -14,6 +14,7 @@
 #include <boost/algorithm/string/split.hpp>
 #include <boost/algorithm/string/find_iterator.hpp>
 #include <boost/range/iterator_range.hpp>
+#include <boost/bind.hpp>
 
 #include "testing/gunit.h"
 
